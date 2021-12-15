b1a5951f3e2b0d17f1e14f65f2a10fbad27f2071
Phelps <phelpsmemo@gmail.com>
2021-12-15 20:08:12 -0300

    update files headers (Closes #3)

--- storm_oauth/user.py
+++ storm_oauth/user.py
@@ -1,10 +1,9 @@
+# -*- coding: utf-8 -*-
 #
-# This file is part of Access Control module for the Storm platform..
-# Copyright (C) 2021 INPE.
+# Copyright (C) 2021 Storm Project.
 #
-# Authentication module for the Storm platform. is free software; you can redistribute it and/or modify it
-# under the terms of the MIT License; see LICENSE file for more details.
-#
+# storm-oauth is free software; you can redistribute it and/or modify it under
+# the terms of the MIT License; see LICENSE file for more details.
 
 from flask import current_app
 
