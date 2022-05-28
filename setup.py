27634041f558a87ae8eccfc1e90c9bb16f57f2f1
Felipe Carlos <efelipecarlos@gmail.com>
2022-05-28 23:35:34 +0000

    invenio-access: defining the supported invenio-accounts version

--- setup.py
+++ setup.py
@@ -53,6 +53,8 @@ install_requires = [
     "Flask-Security>=3.0.0",
     "Flask-Principal>=0.4.0",
     "Werkzeug>=2.0.2",
+    # Invenio dependencies
+    "invenio-accounts<2.0.0",
     # Brazil Data Cube Dependencies
     "bdc-auth-client @ git+https://github.com/brazil-data-cube/bdc-auth-client@v0.2.3",
 ]
